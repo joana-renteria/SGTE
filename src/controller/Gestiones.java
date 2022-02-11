67ba797f3d7c5bcf7e0245b9efbe7af889682754
deketh <dekethxd@gmail.com>
2022-02-11 14:27:08 +0100

    añadido código para modificar

--- src/controller/Gestiones.java
+++ src/controller/Gestiones.java
@@ -1,1 +1,71 @@
-// IMPLEMENTS OPCIONES
+public class Gestiones implements Opciones{
+    private GestorAlumnos gestor;
+
+    public Gestiones(GestorAlumnos gestor){
+        this.gestor = gestor;
+    }
+
+    @Override
+    public void ejecutarByNum(String opc) {
+        switch (opc) {
+            case "1": opc_a(); break;
+            case "2": opc_b(); break;
+            case "3": opc_c(); break;
+            case "4": opc_d(); break;
+            case "5": opc_e(); break;
+            case "6": opc_f(); break;
+            case "7": opc_g(); break;
+            case "8": opc_h(); break;
+            case "9": opc_i(); break;
+            default:
+                break;
+        }
+    }    
+
+    @Override
+    public String[] getDescs() {
+        return new String[]{
+            "Opcion 1",
+            "Opcion 2" // añadir más.
+        };
+    }
+  
+  // LAS OPCIONES QUE SEAN
+    
+    private void opc_a() {
+        
+    }
+
+    private void opc_b() {
+        
+    }
+
+    private void opc_c() {
+        
+    }
+
+    private void opc_d() {
+       
+    }
+
+    private void opc_e() {
+        
+    }
+
+    private void opc_f() {
+        
+    }
+
+    private void opc_g() {
+        
+    }
+
+    private void opc_h() {
+        
+    }
+
+    private void opc_i() {
+        
+        
+    }
+}
